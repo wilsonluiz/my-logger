25792aa5c1ee197b384ada954fd9c2384ef8d7e1
Wilson I Luiz <wilson@t2s.com.br>
2019-04-13 19:51:17 -0300

    Adicionada classe para ajudar a registrar os logs de desempenho

---
+++ MyLogger.Core/RastreadorDesempenho.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLogger.Core
+{
+    public class RastreadorDesempenho
+    {
+        private readonly Stopwatch _sw;
+        private readonly MyLogDetail _infoParaLogar;
+
+        public RastreadorDesempenho(string mensagem, decimal idUsuario, string nomeUsuario,
+            string localizacao, string aplicacao, string camadaAplicacao)
+        {
+            _sw = Stopwatch.StartNew();
+            _infoParaLogar = new MyLogDetail()
+            {
+                Mensagem = mensagem,
+                IdUsuario = idUsuario,
+                NomeUsuario = nomeUsuario,
+                Aplicacao = aplicacao,
+                CamadaAplicacao = camadaAplicacao,
+                Localizacao = localizacao,
+                NomeServidor = Environment.MachineName
+            };
+
+            var inicio = DateTime.Now;
+            _infoParaLogar.InformacaoAdicional = new Dictionary<string, object>()
+            {
+                {"Iniciou", inicio.ToString(CultureInfo.InvariantCulture)}
+            };
+        }
+
+        public RastreadorDesempenho(string mensagem, decimal idUsuario, string nomeUsuario,
+            string localizacao, string aplicacao, string camadaAplicacao,
+            Dictionary<string, object> parametrosDesempenho)
+            : this(mensagem, idUsuario, nomeUsuario, localizacao, aplicacao, camadaAplicacao)
+        {
+            foreach (var parametro in parametrosDesempenho)
+            {
+                _infoParaLogar.InformacaoAdicional.Add("input-" + parametro.Key, parametro.Value);
+            }
+        }
+
+        public void Stop()
+        {
+            _sw.Stop();
+            _infoParaLogar.DuracaoMilessegundos = _sw.ElapsedMilliseconds;
+            MyLogger.LogDesempenho(_infoParaLogar);
+        }
+    }
+}
