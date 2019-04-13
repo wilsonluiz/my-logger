59fd653bfc7607b7944833b668b335d99f3218aa
Wilson I Luiz <wilson@t2s.com.br>
2019-04-13 17:54:39 -0300

    Implementada classe reponsável por armazenar as informações dos slogs

--- MyLogger.Core/MyLogDetail.cs
+++ MyLogger.Core/MyLogDetail.cs
@@ -8,5 +8,49 @@ namespace MyLogger.Core
 {
     public class MyLogDetail
     {
+        public MyLogDetail()
+        {
+            DataHora = DateTime.Now;
+        }
+
+        public DateTime DataHora { get; private set; }
+        public string Mensagem { get; set; }
+
+        #region ONDE
+        public string Aplicacao { get; set; }
+        public string CamadaAplicacao { get; set; }
+        public string Localizacao { get; set; }
+        public string NomeServidor { get; set; }
+        #endregion
+
+        #region QUEM
+        public decimal IdUsuario { get; set; }
+        public string NomeUsuario { get; set; }
+        public decimal IdCliente { get; set; }
+        public string NomeCliente { get; set; }
+        #endregion
+
+        #region INFORMAÇÕES ADICIONAIS
+        /// <summary>
+        /// Utilzada nos logs de desempenho
+        /// </summary>
+        public long? DuracaoMilessegundos { get; set; }
+
+        /// <summary>
+        /// Exceção do log de erro
+        /// </summary>
+        public Exception Excecao { get; set; }
+
+        /// <summary>
+        /// Id que relaciona o cliente com o servidor
+        /// </summary>
+        public decimal IdRelacionado { get; set; }
+
+        /// <summary>
+        /// Armazena informações adicionais que possam ser
+        /// incluídas no log
+        /// </summary>
+        public Dictionary<string, object> InformacaoAdicional { get; set; }
+        #endregion
     }
 }
