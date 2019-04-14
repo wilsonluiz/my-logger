30f98c0669aba7d786c5d6913fc0d716577eac4d
Wilson I Luiz <wilson@t2s.com.br>
2019-04-14 09:05:16 -0300

    Implementado método base para criação do detalhe do log

--- MyLogger.Console/Program.cs
+++ MyLogger.Console/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyLogger.Core;
 
 namespace MyLogger.Console
 {
@@ -10,6 +11,20 @@ namespace MyLogger.Console
     {
         static void Main(string[] args)
         {
+        }
+
+        private static MyLogDetail ObterDetalheDoLog(string mensagem, Exception excecao)
+        {
+            return new MyLogDetail()
+            {
+                Aplicacao = "MyLogger",
+                Localizacao = "MyLogger.Console",
+                CamadaAplicacao = "Job",
+                NomeUsuario = Environment.UserName,
+                NomeServidor = Environment.MachineName,
+                Mensagem = mensagem,
+                Excecao = excecao
+            };
         }
     }
 }
