3cb8c161d26b188a9d86a78a728120d67a4c458a
Wilson I Luiz <wilson@t2s.com.br>
2019-04-14 09:30:50 -0300

    Implemento no programa a execução de todos os tipos de log

--- MyLogger.Console/Program.cs
+++ MyLogger.Console/Program.cs
@@ -11,6 +11,32 @@ namespace MyLogger.Console
     {
         static void Main(string[] args)
         {
+            var detalheLog = ObterDetalheDoLog("inciando a aplicacao", null);
+            Core.MyLogger.LogDiagnostico(detalheLog);
+
+            var ratreador = new RastreadorDesempenho("MyLogger.Console_Execution", null,
+                detalheLog.NomeUsuario, detalheLog.Localizacao, detalheLog.Aplicacao,
+                detalheLog.CamadaAplicacao);
+
+            try
+            {
+                var excecao = new Exception("Algo de ruim aconteceu!");
+                excecao.Data.Add("input param", "nada para ver aqui");
+                throw excecao;
+            }
+            catch (Exception ex)
+            {
+                detalheLog = ObterDetalheDoLog("", ex);
+                Core.MyLogger.LogErro(detalheLog);
+            }
+
+            detalheLog = ObterDetalheDoLog("utilizado mylloger.console", null);
+            Core.MyLogger.LogUtilizacao(detalheLog);
+
+            detalheLog = ObterDetalheDoLog("parando a aplicacao", null);
+            Core.MyLogger.LogDiagnostico(detalheLog);
+
+            ratreador.Stop();
         }
 
         private static MyLogDetail ObterDetalheDoLog(string mensagem, Exception excecao)
