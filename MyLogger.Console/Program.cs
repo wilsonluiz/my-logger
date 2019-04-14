70c1658427f0c8565fd1b7ead3d0a0bb2d83cf75
Wilson I Luiz <wilson@t2s.com.br>
2019-04-14 08:59:00 -0300

    Adicionado projeto MyLogger.Console com referência ao framework de log criado

---
+++ MyLogger.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLogger.Console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
